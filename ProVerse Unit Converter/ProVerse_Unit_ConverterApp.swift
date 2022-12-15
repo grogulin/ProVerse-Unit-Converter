aed3189a1c7abb460dc6bc1342d989c821f0cde4
Yaroslav Grogul <time7bass@gmail.com>
2022-12-15 13:29:54 +0100

    Initial Commit

---
+++ ProVerse Unit Converter/ProVerse_Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProVerse_Unit_ConverterApp.swift
+//  ProVerse Unit Converter
+//
+//  Created by Ярослав Грогуль on 15.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ProVerse_Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
